a4c16e9dbb99c5d10cb99ca0c21c3534a8256dc8
adamjatim <adamrahmatilahi@gmail.com>
2022-09-23 03:52:23 +0700

    update

---
+++ config/i3/autostart.sh
@@ -0,0 +1,21 @@
+
+# Autostart applications
+# /usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1 &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+# nitrogen --restore; sleep 1; 
+feh --bg-fill ~/.config/backgrounds/sipaling-aestethics.png &
+picom -b &
+numlockx on &
+nm-applet &
+volumeicon &
+dunst &
+# blueman-applet &
+
+# sxhkd
+sxhkd -c ~/.config/i3/sxhkd/sxhkdrc &
+
+# increst audio to 150%
+pactl set-sink-volume @DEFAULT_SINK@ 150% &
+
+# set input audio to 35%
+pactl set-source-volume @DEFAULT_SOURCE@ 30%
